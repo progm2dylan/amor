2a6742f7e7184719f5433f56438f705155995a63
progm2dylan <87798200+progm2dylan@users.noreply.github.com>
2021-08-23 22:09:09 -0300

    Create Fracaso.py

---
+++ Fracaso.py
@@ -0,0 +1,162 @@
+import os
+import time
+print("""
+
+
+
+
+
+
+
+
+
+┬  ┌─┐  ┌─┐┌─┐┌─┐┬─┐      
+│  ├─┤  ├─┘├┤ │ │├┬┘      
+┴─┘┴ ┴  ┴  └─┘└─┘┴└─      
+╦  ╦┬┌┬┐┌─┐               
+╚╗╔╝│ ││├─┤               
+ ╚╝ ┴─┴┘┴ ┴               
+╔╦╗┌─┐  ┬  ┌─┐┌─┐         
+ ║║├┤   │  │ │└─┐         
+═╩╝└─┘  ┴─┘└─┘└─┘         
+╦ ╦┌┐┌┬┬  ┬┌─┐┬─┐┌─┐┌─┐┌─┐
+║ ║││││└┐┌┘├┤ ├┬┘└─┐│ │└─┐
+╚═╝┘└┘┴ └┘ └─┘┴└─└─┘└─┘└─┘
+
+
+
+LA PEOR VIDA:)
+
+fuck you
+""")
+for animo in range(999):
+	print("""
+	m
+	i
+	e
+	r
+	d
+	a
+	""")
+print("no")
+time.sleep(0.2)
+print("se")
+time.sleep(0.2)
+print("puede")
+time.sleep(0.2)
+print("confiar")
+time.sleep(0.2)
+print("en")
+time.sleep(0.2)
+print("nadie.")
+time.sleep(4)
+for i in range(700):
+	print("""┬  ┌─┐  ┌─┐┌─┐┌─┐┬─┐      
+│  ├─┤  ├─┘├┤ │ │├┬┘      
+┴─┘┴ ┴  ┴  └─┘└─┘┴└─      
+╦  ╦┬┌┬┐┌─┐               
+╚╗╔╝│ ││├─┤               
+ ╚╝ ┴─┴┘┴ ┴               
+╔╦╗┌─┐  ┬  ┌─┐┌─┐         
+ ║║├┤   │  │ │└─┐         
+═╩╝└─┘  ┴─┘└─┘└─┘         
+╦ ╦┌┐┌┬┬  ┬┌─┐┬─┐┌─┐┌─┐┌─┐
+║ ║││││└┐┌┘├┤ ├┬┘└─┐│ │└─┐
+╚═╝┘└┘┴ └┘ └─┘┴└─└─┘└─┘└─┘""")
+os.system("clear")
+print("""
+
+    
+        
+            
+                
+                    
+                        
+                            
+                                
+                                    _______________¶¶¶¶¶¶
+__________¶¶¶¶¶¶¶¶__¶¶
+_____¶¶¶¶¶¶¶¶¶¶¶¶¶¶ ¶¶
+_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+¶¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+_¶¶ ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+___ ¶¶¶¶¶¶¶¶¶¶¶ [█]¶¶¶¶¶
+____¶¶¶¶¶ [█] ¶¶¶¶¶¶¶¶¶¶
+  _____¶¶¶¶¶¶¶`▼´¶¶¶¶¶¶
+   _____¶¶¶¶¶¶¶·♥·¶¶¶¶¶
+__¤'¤'¤'¤'¤'¤'¤¶¶¶¶¶¶¤'¤'¤'¤'¤'¤'¤
+__¶¶¶¶¶'¤'¤'¤'¤'¤I T.'¤'¤'¤'¶¶¶¶¶¶
+¶¶¶¶¶¶¶¶¤'¤'¤'¤'¤'¤'¤'¤'¤¶¶¶¶¶¶¶¶
+¶¶____ ¶¶¶¶¶'¤'¤'¤'¤'¶¶¶¶¶____ ¶¶
+¶¶_____ ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶____¶¶
+_¶¶___¶¶¶¶¶________¶¶¶¶___¶¶
+__¶¶¶¶¶¶¶¶___________¶¶¶¶¶¶
+
+    ERROR ERROR DETECTADO EN EL ALMA:/
+    
+        
+""")
+def v():
+	print("Nunca confies en nadie, el amor no existe.")   
+
+time.sleep(0.2)
+print("Sad for ever")
+v()    
+time.sleep(2)     
+print("si tu no le haces infiel a ella ella te hara infiel a ti, asi es la vida amigo, no te dejes engañar.")
+print("")
+def amor():
+	print("no confies en nadie")
+os.system("clear")
+time.sleep(2)
+print("si estas con un hombre y te gusta otro no lo engañes las personas tienen sentimientos osea, solo dejalo no hace falta engañarlo lo lastima el triple imaginate si esa persona eras tu... no lo hagas amiga, esta leccion va para los hombres.")
+def dylan():
+	print("""
+	
+	
+	
+	
+    
+        
+            
+                
+                    
+                        
+                            
+                                
+                                    _______________¶¶¶¶¶¶
+__________¶¶¶¶¶¶¶¶__¶¶
+_____¶¶¶¶¶¶¶¶¶¶¶¶¶¶ ¶¶
+_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+¶¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+_¶¶ ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+___ ¶¶¶¶¶¶¶¶¶¶¶ [█]¶¶¶¶¶
+____¶¶¶¶¶ [█] ¶¶¶¶¶¶¶¶¶¶
+  _____¶¶¶¶¶¶¶`▼´¶¶¶¶¶¶
+   _____¶¶¶¶¶¶¶·♥·¶¶¶¶¶
+__¤'¤'¤'¤'¤'¤'¤¶¶¶¶¶¶¤'¤'¤'¤'¤'¤'¤
+__¶¶¶¶¶'¤'¤'¤'¤'¤I T.'¤'¤'¤'¶¶¶¶¶¶
+¶¶¶¶¶¶¶¶¤'¤'¤'¤'¤'¤'¤'¤'¤¶¶¶¶¶¶¶¶
+¶¶____ ¶¶¶¶¶'¤'¤'¤'¤'¶¶¶¶¶____ ¶¶
+¶¶_____ ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶____¶¶
+_¶¶___¶¶¶¶¶________¶¶¶¶___¶¶
+__¶¶¶¶¶¶¶¶___________¶¶¶¶¶¶
+
+    ERROR ERROR DETECTADO EN EL ALMA:/
+    
+    
+    
+   
+   
+   
+   """)
+time.sleep(4)   
+print("t/t/t")
+print("en fin si no le haces infiel a ella ella te hara a ti")
+time.sleep(4)
+dylan()
+print("Las personas que quieres te rechazan. Las personas que no las quieres te aman y tu no los aceptas...")
+time.sleep(5)
+for i in range(5):
+	print("mi estado de animo:/")
+	
